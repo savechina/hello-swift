4fb0ba86da8c58f33e7839015a26d433196af142
RenYan Wei <weirenyan@hotmail.com>
2024-12-18 00:16:24 +0800

    feat: init project

---
+++ hello-swift/Basic.swift
@@ -0,0 +1,78 @@
+//
+//  Basic.swift
+//  hello-swift
+//  Basic module
+//  Created by RenYan Wei on 2024/12/16.
+//
+
+import Foundation
+
+/// basic express sample
+func expressionSample() {
+    //define variable number
+    var n = 42
+    n = 50
+
+    //
+    let m = 42
+
+    n = n + m
+
+    //print
+    print("sum(n+m):", n)
+
+    let label = "The width is "
+    let width = 94
+    let widthLabel = label + String(width)
+
+    print(widthLabel)
+}
+
+/// basic string sample
+func stringSample() {
+    print("\nbasic string sample start ...")
+    let apples = 3
+    let oranges = 5
+    let appleSummary = "I have \(apples) apples."
+    let fruitSummary = "I have \(apples + oranges) pieces of fruit."
+
+    print("appleSummary:", appleSummary)
+
+    print("fruitSummary:", fruitSummary)
+
+    //string function return result
+    let result = greet(person: "Bob", day: "Tuesday")
+
+    print(result)
+
+    print("basic string sample end.\n")
+
+}
+
+/// greet say hello
+/// - Parameters:
+///   - person: person name
+///   - day: today
+/// - Returns:  result
+func greet(person: String, day: String) -> String {
+    return "Hello \(person), today is \(day)."
+}
+
+func structSample() {
+    //simpleClass implement ExampleProtocol
+    let simple = SimpleClass()
+    simple.adjust()
+
+    let aDescription = simple.simpleDescription
+    print(aDescription)
+
+    //simpleStruct
+    var simpleStruct = SimpleStructure()
+    simpleStruct.adjust()
+
+    let bDescription = simpleStruct.simpleDescription
+    print(bDescription)
+
+    // Prints "The number 7"
+    print(7.simpleDescription)
+}
---
+++ hello-swift/ExampleProtocol.swift
@@ -0,0 +1,38 @@
+//
+//  ExampleProtocol.swift
+//  hello-swift
+//
+//  Created by RenYan Wei on 2024/12/15.
+//
+
+/// protocol ExampleProtocol
+protocol ExampleProtocol {
+    var simpleDescription: String { get }
+    mutating func adjust()
+}
+
+///SimpleClass implement ExampleProtocol
+class SimpleClass: ExampleProtocol {
+    var simpleDescription: String = "A very simple class."
+    var anotherProperty: Int = 69105
+    func adjust() {
+        simpleDescription += "  Now 100% adjusted."
+    }
+}
+
+/// SimpleStructure
+struct SimpleStructure: ExampleProtocol {
+    var simpleDescription: String = "A simple structure"
+    mutating func adjust() {
+        simpleDescription += " (adjusted)"
+    }
+}
+/// Int extension ExampleProtocol
+extension Int: ExampleProtocol {
+    var simpleDescription: String {
+        return "The number \(self)"
+    }
+    mutating func adjust() {
+        self += 42
+    }
+}
